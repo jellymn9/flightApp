3a7f3e086fb9f9b72d5f5931c1f9f4a5dc9866e1
Jelena <jelena.mandzic@gmail.com>
2022-04-24 18:56:51 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 # https://github.com/jellymn9/flightApp
-git push -f git@github.com:jellymn9/flightApp.git master:gh-pages
+git push -f git@github.com:jellymn9/flightApp.git main:gh-pages
 
 
 cd -
